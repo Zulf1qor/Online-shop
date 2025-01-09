fdf5c0a91c7a8407a10845b557939249992f26b1
Javohir-mirolimov <githup@gmail.com>
2025-01-09 20:14:21 +0500

    javohir

--- main/models.py
+++ main/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    mobile_number = models.CharField(max_length=20, null=True, blank=True)
+    address = models.CharField(max_length=255)
 
+    class Meta:
+        swappable = 'AUTH_USER_MODEL'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.username
 class Banner(models.Model):
     title = models.CharField(max_length=55)
     addition = models.CharField(max_length=25)
